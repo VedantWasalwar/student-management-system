1784c2cf053abee2cd5432dbc3b36799f350c8cb
Vedant_Wasalwar <vedantwasalwar43@gmail.com>
2026-03-01 00:51:34 +0530

    01-03-26

--- config.php
+++ config.php
@@ -1,5 +1,11 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "student_db");
+$conn = new mysqli(
+    "sql307.infinityfree.com",   // MySQL Hostname
+    "if0_41273611",              // MySQL Username
+    "Vedant2026",                // MySQL Password
+    "if0_41273611_smsdatabase",  // MySQL Database Name
+    3306                         // MySQL Port
+);
 
 if ($conn->connect_error) {
     die("Connection Failed: " . $conn->connect_error);
